8dbe019d387af4dea472688265c9f670090e916b
Lê Minh Quân <minhquan0963580006@gmail.com>
2026-02-24 13:57:51 +0700

    Refactor pagination structure in findAll method for improved readability

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -5,18 +5,22 @@ class PostService {
 		const limit = 7;
 		const offset = (page - 1) * limit;
 		const posts = await postModel.findAll(limit, offset);
-
 		const totalPost = await postModel.count();
+
+		const pagination = {
+			total: totalPost,
+			per_page: limit,
+			current_page: page,
+		};
+
+		if (posts.length) {
+			pagination.from = offset + 1;
+			pagination.to = offset + posts.length;
+		}
 
 		return {
 			rows: posts,
-			pagination: {
-				total: totalPost,
-				per_page: limit,
-				current_page: page,
-				from: offset + 1,
-				to: offset + posts.length,
-			},
+			pagination,
 		};
 	}
 
